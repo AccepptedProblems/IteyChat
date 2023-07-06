95b71d5bb31d94a4b150bdc6ba243e12fc706f87
ThanhNam <kidpooh2k@gmail.com>
2023-07-06 13:35:56 +0700

    First commit

---
+++ src/main/java/com/chatapp/itey/IteyApplication.java
@@ -0,0 +1,12 @@
+package com.chatapp.itey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IteyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/itey/IteyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.itey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
